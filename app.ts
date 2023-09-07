fa717e6525c9d32615d862a1153bb9add526f8c1
EmmanuelKeifala <emmanuelkeifala@gmail.com>
2023-09-07 21:04:19 +0000

    fresh

--- app.ts
+++ app.ts
@@ -1,4 +1,40 @@
-import express from "express"
+/** @format */
 
-export const app = express()
+// Package imports
+import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
+require("dotenv").config();
+import cookieParser from "cookie-parser";
 
+
+
+export const app = express();
+
+// Body parser
+app.use(express.json({ limit: "50mb" }));
+
+// Cookie parser
+app.use(cookieParser());
+
+// Cors <=> protecting our apis by origins
+app.use(
+	cors({
+		origin: process.env.ORIGIN,
+	}),
+);
+
+// Testing the API
+app.get("/test", (req: Request, res: Response, next: NextFunction) => {
+	res.status(200).json({
+		success: true,
+		message: "APi is working",
+	});
+});
+
+// Unknow api route request
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+	const error = new Error(`Route ${req.originalUrl} not found`) as any;
+
+	error.statusCode = 404;
+	next(error);
+});
--- server.ts
+++ server.ts
@@ -1,8 +1,13 @@
 /** @format */
 
+//
 import { app } from "./app";
 require("dotenv").config();
 
+// Files and utils imports
+import connectDB from "./utils/db";
+
+connectDB();
 // Create server
 app.listen(process.env.PORT, () => {
 	console.log(`Server is connected on PORT: ${process.env.PORT}`);
